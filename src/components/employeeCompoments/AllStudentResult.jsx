7805c9609ffa1306bb72f8a5a983025c657e167b
Scholarsdenweb <webdeveloper@scholarsden.in>
2025-02-27 16:49:28 +0530

    remove extra button

--- src/components/employeeCompoments/AllStudentResult.jsx
+++ src/components/employeeCompoments/AllStudentResult.jsx
@@ -4,47 +4,6 @@ import Navbar from "../Form/Navbar";
 import axios from "../../api/axios";
 
 const AllStudentResult = () => {
-  const [file, setFile] = useState(null);
-  const [uploadStatus, setUploadStatus] = useState("");
-
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-  };
-
-  const handleGenerateResult = async () => {
-    try {
-      const responce = await axios.get("/employees/generate-zip");
-      console.log("responce", responce);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-//   const handleDownloadResult = async () => {
-//     try {
-//       const response = await axios.get("/employees/download-zip");
-
-
-
-
-    
-//       const blob = await response.blob(); // Convert response to blob
-//       console.log("blob",blob);
-//       const url = window.URL.createObjectURL(blob); // Create download URL
-//       const a = document.createElement("a");
-//       a.href = url;
-//       a.download = "student_results.zip"; // Set download file name
-//       document.body.appendChild(a);
-//       a.click();
-//       a.remove();
-//       window.URL.revokeObjectURL(url); // Clean up URL
-//     } catch (error) {
-//         console.log("error",error);
-//       alert("Error downloading ZIP file.",error);
-//     }
-//   };
-
-
-
 
   const handleDownloadResult = async () => {
     try {
@@ -80,36 +39,7 @@ const AllStudentResult = () => {
 
 
 
-  const handleUpload = async () => {
-    if (!file) {
-      setUploadStatus("Please select a file first.");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("csvFile", file);
-
-    formData.forEach((value, key) => {
-      console.log("FormData Key:", key, "=>", value);
-    });
-
-    try {
-      const response = await axios.post("/employees/generateResult", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-
-      console.log("response from employee dashboard", response);
-
-      if (response.status === 200) {
-        setUploadStatus(`Upload successful: ${response.data.message}`);
-      } else {
-        setUploadStatus("Upload failed. Please try again.");
-      }
-    } catch (error) {
-      console.error("Upload error:", error);
-      setUploadStatus("Error uploading file.");
-    }
-  };
+ 
 
   return (
     <div
@@ -129,12 +59,7 @@ const AllStudentResult = () => {
           <div className="col-span-6 px-9 py-8 mb-3 mr-5 h-full bg-gray-100 rounded-3xl flex flex-col items-center justify-center gap-4 overflow-auto">
             {/* File Input */}
 
-            <button
-              className="p-3 bg-blue-500 text-white"
-              onClick={handleGenerateResult}
-            >
-              Generate All Result
-            </button>
+           
 
             <button
               className="p-3 bg-blue-500 text-white"
